f2f0ed8f415507049d42431c6cde8a059f53d009
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-07-27 11:11:26 +0500

    Add transition to loader.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ loader = document.querySelector(".lds-spinner-wrapper");
 
 document.addEventListener("DOMContentLoaded", function(){
   setTimeout(function() {
+    loader.classList.add("lds-spinner-wrapper--opacity");
+  }, 1000);
+  setTimeout(function() {
     loader.classList.add("lds-spinner-wrapper--none");
-  }, 900);
+  }, 1300);
 });
