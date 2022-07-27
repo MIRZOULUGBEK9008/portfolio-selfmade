8db233039ce47f4e0fe98a9c2c0dc5de95119c21
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-07-27 15:12:05 +0500

    Upload all projects image. Add modifiers to JS.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,42 @@
-const toggle = document.querySelector(".toggle"),
-loader = document.querySelector(".lds-spinner-wrapper");
+// Modifiers
+const modifiers = {
+  dark: "dark",
+  white: "oq",
+  black: "qora"
+}
 
+const elLoader = document.querySelector(".lds-spinner-wrapper"),
+theme = localStorage.getItem("theme", "dark"),
+darkModeToggle = document.querySelector(".js-toggle");
+
+// Loader
 document.addEventListener("DOMContentLoaded", function(){
   setTimeout(function() {
-    loader.classList.add("lds-spinner-wrapper--opacity");
+    elLoader.classList.add("lds-spinner-wrapper--opacity");
   }, 1000);
   setTimeout(function() {
-    loader.classList.add("lds-spinner-wrapper--none");
+    elLoader.classList.add("lds-spinner-wrapper--none");
   }, 1300);
+  if (theme === "dark") {
+    document.body.classList.add(modifiers.dark);
+    darkModeToggle.textContent = modifiers.white;
+  } else {
+    document.body.classList.remove(modifiers.dark);
+    darkModeToggle.textContent = modifiers.black;
+  }
 });
+
+// Toggle
+  darkModeToggle.addEventListener("click", function () {
+    document.body.classList.toggle(modifiers.dark);
+    if (document.body.classList.contains(modifiers.dark)) {
+      localStorage.setItem("theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+    }
+    if (darkModeToggle.textContent === modifiers.black) {
+      darkModeToggle.textContent = modifiers.white;
+    } else {
+      darkModeToggle.textContent = modifiers.black;
+    }
+  });
