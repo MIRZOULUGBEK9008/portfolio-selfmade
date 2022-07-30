8de8f20b465d9cd2005a1bc3c45030728e04c407
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-07-30 21:28:49 +0500

    Add transition timeout more .

--- js/main.js
+++ js/main.js
@@ -18,6 +18,23 @@ elScrollTop = document.querySelector(".js-scroll"),
 elLogo = document.querySelector(".logo"),
 elSiteHeaderContentWrapper = document.querySelector(".site-header__img-content-wrapper");
 
+// Loader
+document.addEventListener("DOMContentLoaded", function(){
+  setTimeout(function() {
+    elLoader.classList.add("lds-spinner-wrapper--opacity");
+  }, 1000);
+  setTimeout(function() {
+    elLoader.classList.add("lds-spinner-wrapper--none");
+  }, 1500);
+  if (theme === "dark") {
+    document.body.classList.add(modifiers.dark);
+    elDarkModeToggle.textContent = modifiers.white;
+  } else {
+    document.body.classList.remove(modifiers.dark);
+    elDarkModeToggle.textContent = modifiers.black;
+  }
+});
+
 // Add shadow
 window.addEventListener("scroll", function() {
   let element = window.scrollY;
@@ -33,23 +50,6 @@ window.addEventListener("scroll", function() {
     elLogo.classList.remove(modifiers.logo);
     elScrollTop.classList.remove(modifiers.scrollTop);
     elDarkModeToggle.classList.remove(modifiers.toggler);
-  }
-});
-
-// Loader
-document.addEventListener("DOMContentLoaded", function(){
-  setTimeout(function() {
-    elLoader.classList.add("lds-spinner-wrapper--opacity");
-  }, 1000);
-  setTimeout(function() {
-    elLoader.classList.add("lds-spinner-wrapper--none");
-  }, 1300);
-  if (theme === "dark") {
-    document.body.classList.add(modifiers.dark);
-    elDarkModeToggle.textContent = modifiers.white;
-  } else {
-    document.body.classList.remove(modifiers.dark);
-    elDarkModeToggle.textContent = modifiers.black;
   }
 });
 
