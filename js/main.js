2f9257034ed120605fd9ceab42fb1b8f9e407674
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-07-31 16:17:38 +0500

    Add hamburger menu and refactor it.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,36 @@
 // Modifiers
 const modifiers = {
   dark: "dark",
-  white: "oq",
-  black: "qora",
+  active: "toggler__img--active",
   scroll: "site-header--scroll",
   content: "site-header__img-content-wrapper--none",
   toggler: "toggler--block",
   scrollTop: "scroll-site-top--block",
-  logo: "logo--block"
+  block: "hamburger__bar--block"
 }
 
 const elLoader = document.querySelector(".lds-spinner-wrapper"),
 theme = localStorage.getItem("theme", "dark"),
 elDarkModeToggle = document.querySelector(".js-toggle"),
+elDarkModeToggleImgMoon = document.querySelector(".toggler__img--moon"),
+elDarkModeToggleImgSun = document.querySelector(".toggler__img--sun"),
 elSiteHeader = document.querySelector(".site-header"),
 elScrollTop = document.querySelector(".js-scroll"),
-elLogo = document.querySelector(".logo"),
+elsId = document.querySelectorAll(".js-id"),
+elHamburger = document.querySelector(".js-hamburger"),
+elHamburgerToggle = document.querySelector(".js-hamburger-toggle"),
 elSiteHeaderContentWrapper = document.querySelector(".site-header__img-content-wrapper");
+
+// Function
+function removeActiveAddActive() {
+  elDarkModeToggleImgMoon.classList.remove(modifiers.active);
+  elDarkModeToggleImgSun.classList.add(modifiers.active);
+}
+
+function addActiveRemoveActive() {
+  elDarkModeToggleImgMoon.classList.add(modifiers.active);
+  elDarkModeToggleImgSun.classList.remove(modifiers.active);
+}
 
 // Loader
 document.addEventListener("DOMContentLoaded", function(){
@@ -28,10 +42,10 @@ document.addEventListener("DOMContentLoaded", function(){
   }, 1500);
   if (theme === "dark") {
     document.body.classList.add(modifiers.dark);
-    elDarkModeToggle.textContent = modifiers.white;
+    removeActiveAddActive();
   } else {
     document.body.classList.remove(modifiers.dark);
-    elDarkModeToggle.textContent = modifiers.black;
+    addActiveRemoveActive();
   }
 });
 
@@ -41,15 +55,15 @@ window.addEventListener("scroll", function() {
   if (element > 0) {
     elSiteHeader.classList.add(modifiers.scroll);
     elSiteHeaderContentWrapper.classList.add(modifiers.content);
-    elLogo.classList.add(modifiers.logo);
     elScrollTop.classList.add(modifiers.scrollTop);
     elDarkModeToggle.classList.add(modifiers.toggler);
+    elHamburgerToggle.classList.add(modifiers.block);
   } else {
     elSiteHeader.classList.remove(modifiers.scroll);
     elSiteHeaderContentWrapper.classList.remove(modifiers.content);
-    elLogo.classList.remove(modifiers.logo);
     elScrollTop.classList.remove(modifiers.scrollTop);
     elDarkModeToggle.classList.remove(modifiers.toggler);
+    elHamburgerToggle.classList.remove(modifiers.block);
   }
 });
 
@@ -61,9 +75,20 @@ elDarkModeToggle.addEventListener("click", function () {
   } else {
     localStorage.setItem("theme", "light");
   }
-  if (elDarkModeToggle.textContent === modifiers.black) {
-    elDarkModeToggle.textContent = modifiers.white;
-  } else {
-    elDarkModeToggle.textContent = modifiers.black;
-  }
+  elDarkModeToggleImgMoon.classList.toggle(modifiers.active);
+  elDarkModeToggleImgSun.classList.toggle(modifiers.active);
+});
+
+
+
+// Hamburger
+elHamburgerToggle.addEventListener("click", function () {
+  elHamburger.classList.toggle("hamburger__sitenav--active");
+});
+
+
+elsId.forEach(function (id) {
+  id.addEventListener("click", function () {
+    elHamburger.classList.toggle("hamburger__sitenav--active");
+  });
 });
